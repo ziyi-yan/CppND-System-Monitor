17989b89c8cd3bd402d3c910906e163b5d11eb82
Ziyi Yan <ziyi.yan@foxmail.com>
2019-07-05 22:48:37 +0800

    complete ProcessParser and Process

--- Process.h
+++ Process.h
@@ -18,18 +18,17 @@ public:
     Process(string pid){
         this->pid = pid;
         this->user = ProcessParser::getProcUser(pid);
-        //TODOs:
-        //complete for mem
-        //complete for cmd
-        //complete for upTime
-        //complete for cpu
+        this->mem = ProcessParser::getVmSize(pid);
+        this->cmd = ProcessParser::getCmd(pid);
+        this->upTime = ProcessParser::getProcUpTime(pid);
+        this->cpu = ProcessParser::getCpuPercent(pid);
     }
     void setPid(int pid);
     string getPid()const;
     string getUser()const;
     string getCmd()const;
-    int getCpu()const;
-    int getMem()const;
+    string getCpu()const;
+    string getMem()const;
     string getUpTime()const;
     string getProcess();
 };
@@ -39,12 +38,39 @@ void Process::setPid(int pid){
 string Process::getPid()const {
     return this->pid;
 }
+string Process::getUser()const {
+    return this->user;
+}
+string Process::getCmd()const{
+    return this->cmd;
+}
+string Process::getCpu()const{
+    return this->cpu;
+}
+string Process::getMem()const{
+    return this->mem;
+}
+string Process::getUpTime()const{
+    return this->upTime;
+}
 string Process::getProcess(){
     if(!ProcessParser::isPidExisting(this->pid))
         return "";
-    this->mem = ProcessParser::getVmSize(this->pid);
-    this->upTime = ProcessParser::getProcUpTime(this->pid);
-    this->cpu = ProcessParser::getCpuPercent(this->pid);
+        this->user = ProcessParser::getProcUser(this->pid);
+        this->mem = ProcessParser::getVmSize(this->pid);
+        this->cmd = ProcessParser::getCmd(this->pid);
+        this->upTime = ProcessParser::getProcUpTime(this->pid);
+        this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " + //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+    return (this->pid + "   "
+                    + this->user
+                    + "   "
+                    + this->mem.substr(0,5)
+                    + "     "
+                    + this->cpu.substr(0,5)
+                    + "     "
+                    + this->upTime.substr(0,5)
+                    + "    "
+                    + this->cmd.substr(0,30)
+                    + "...");
 }
